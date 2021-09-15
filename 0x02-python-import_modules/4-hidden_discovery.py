a7c5beba61fcdc88e03e2453abe069983b6e0639
MugoBrian <mikanmuchiri@gmail.com>
2021-09-15 14:30:55 +0300

    Corrected

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
-q#!/usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    list = dir(hidden_4)
-    for i in (list):
-        if i[0] != '_' and i[1] != '_':
-            print(i)
+    names = dir(hidden_4)
+    avoid = "__"
+    for i in range(0, len(all_dir)):
+        if avoid not in all_dir[i]:
+            print(all_dir[i])
